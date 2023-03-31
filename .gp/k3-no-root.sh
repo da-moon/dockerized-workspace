3dc63ec73eab4f18a4dd2939ee3271a567f32a93
da-moon <contact@havi.dev>
2023-03-31 09:34:13 -0400

    fix(k3): add ssh timeout

--- .gp/k3-no-root.sh
+++ .gp/k3-no-root.sh
@@ -5,7 +5,7 @@ set -xeuo pipefail ;
 VM_NAME="k3s" ;
 workdir="${HOME}/qemu" ;
 dir="${workdir}" ;
-qcow="${dir}/arch.qcow2" ;
+qcow="${dir}/${VM_NAME}.qcow2" ;
 iso="${dir}/cloud-init.iso"
 # url for bootable Archlinux VM disk
 url="https://geo.mirror.pkgbuild.com/images/latest/Arch-Linux-x86_64-cloudimg.qcow2" ;
@@ -62,21 +62,26 @@ rm -rf "${dir}" ;
 pushd "${dir}" > /dev/null 2>&1 ;
 
 if [ ! -r "${qcow}" ]; then
-	if [ "$(command -v "aria2c")" != "" ];then
-		aria2c --optimize-concurrent-downloads -k 1M -j16 -x 16 -c --file-allocation=falloc --dir="${dir}" --out="$(basename ${qcow})" "${url}"
-	else
-		wget -qO "${qcow}" "${url}" ;
-	fi
+	# if [ "$(command -v "aria2c")" != "" ];then
+	# 	aria2c --optimize-concurrent-downloads --min-split-size=1M --max-connection-per-server=16 --continue=true --file-allocation=falloc --dir="${dir}" --out="$(basename ${qcow})" "${url}"
+	# else
+		# short form :
+		# wget -nv -qO "${qcow}" "${url}"
+		wget --quiet --no-verbose --show-progress --output-document "${qcow}" "${url}" ;
+	# fi
 fi
 # generating ssh key to inject into the guest 
-[ ! -d "${HOME}/.ssh" ] && mkdir -p "${HOME}/.ssh"
-[ ! -r "${HOME}/.ssh/id_rsa_archlinux" ] && ssh-keygen -b 4096 -t rsa -f "${HOME}/.ssh/id_rsa_archlinux" -N ""
-chmod 700 "${HOME}/.ssh" ;
-find "${HOME}/.ssh" -type f -name "id*" -exec chmod 600 {} \;
-find "${HOME}/.ssh" -type f -name "id*.pub" -exec chmod 644 {} \;
-[ -r "${HOME}/.ssh/authorized_keys" ] && chmod 644 "${HOME}/.ssh/authorized_keys" ;
-[ -r "${HOME}/.ssh/known_hosts" ] && chmod 644 "${HOME}/.ssh/known_hosts" ;
-[ -r "${HOME}/.ssh/config" ] && chmod 644 "${HOME}/.ssh/config" ;
+PRIVATE_KEY="${HOME}/.ssh/id_rsa_${VM_NAME}" ;
+if [ ! -r "${PRIVATE_KEY}" ];then
+	[ ! -d "$(dirname "${PRIVATE_KEY}")" ] && mkdir -p "$(dirname "${PRIVATE_KEY}")"]
+	ssh-keygen -b "4096" -t "rsa" -f "${PRIVATE_KEY}" -N ""
+	chmod 700 "$(dirname "${PRIVATE_KEY}")" ;
+	find "$(dirname "${PRIVATE_KEY}")" -type f -name "id*" -exec chmod 600 {} \;
+	find "$(dirname "${PRIVATE_KEY}")" -type f -name "id*.pub" -exec chmod 644 {} \;
+	[ -r "$(dirname "${PRIVATE_KEY}")/authorized_keys" ] && chmod 644 "$(dirname "${PRIVATE_KEY}")/authorized_keys" ;
+	[ -r "$(dirname "${PRIVATE_KEY}")/known_hosts" ] && chmod 644 "$(dirname "${PRIVATE_KEY}")/known_hosts" ;
+	[ -r "$(dirname "${PRIVATE_KEY}")/config" ] && chmod 644 "$(dirname "${PRIVATE_KEY}")/config" ;
+fi
 if [ ! -r "${iso}" ]; then
 	if [ "$(command -v "go")" != "" ];then
 		if [ "$(command -v "yamlfmt")" == "" ];then
@@ -87,7 +92,7 @@ if [ ! -r "${iso}" ]; then
 # NOTE: location in guest
 # sudo cat /var/lib/cloud/instance/user-data.txt
 # sudo cat /var/lib/cloud/instances/nocloud/cloud-config.txt
-	cat << EOF | tee "${dir}/user-data" > /dev/null
+	cat << EOF | tr -dC '[:print:]\t\n' | tee "${dir}/user-data" > /dev/null
 #cloud-config
 output: {all: '| tee -a /var/log/cloud-init-output.log'}
 disable_root: true
@@ -117,7 +122,7 @@ users:
       - adm
     shell: /bin/bash
     ssh_authorized_keys:
-      - $(cat "${HOME}/.ssh/id_rsa.pub")
+      - $(cat "${PRIVATE_KEY}")
 packages:
   - zsh
 write_files:
@@ -154,6 +159,9 @@ if [ "$(qemu-img info "${qcow}" | grep "virtual size" | awk "{print $3}")" -le 1
 	qemu-img resize "${qcow}" "+10G" ;
 fi
 # calculate VM Memory:
+# alternative to:
+# awk '/MemFree/ { printf "%.3f \n", $2/1024/1024 }' /proc/meminfo
+# https://stackoverflow.com/questions/34937580/get-available-memory-in-gb-using-single-bash-shell-command
 VM_MEM=$(($(getconf _AVPHYS_PAGES) * $(getconf PAGE_SIZE) / (1024 * 1024 * 1024)))
 # If there is less than 2 GB of available memory, exit as we cannot launch the VM
 if [ $VM_MEM -lt 2 ]; then
@@ -169,9 +177,10 @@ fi
 if [ $VM_MEM -lt 2 ]; then
 	VM_MEM=2
 fi
+VM_MEM=2;
 # Allocate a quarter of available logical CPU cores for the VM
 VM_CORES=$(("$(getconf _NPROCESSORS_ONLN)" / 4))
-echo "*** starting "${VM_NAME}" archlinux VM with "qemu". It can take up to ten minutes for "cloud-init" to finish configuration and installation of packages"
+echo "*** starting "${VM_NAME}" archlinux based VM with "qemu". It can take up to ten minutes for "cloud-init" to finish configuration and installation of packages"
 # ──────────────────────────────────────────────────────────────────────────────
 # snippets
 # List all starred vms with
@@ -195,34 +204,35 @@ echo "*** starting "${VM_NAME}" archlinux VM with "qemu". It can take up to ten 
 # 
 # ssh -p 2222 -m "hmac-sha2-512" -o "StrictHostKeyChecking=no" -o "CheckHostIP=no" -o "UserKnownHostsFile=/dev/null" 127.0.0.1
 # ──────────────────────────────────────────────────────────────────────────────
-# -nographic `# disable graphical output and redirect serial I/Os to console` \
-# -serial "mon:stdio" `# get ctrl-c to work` \
+	# -serial "mon:stdio" `# get ctrl-c to work` \
 qemu-system-x86_64 \
 	-daemonize \
+	-nographic `# disable graphical output and redirect serial I/Os to console` \
 	-display "none" \
-	-monitor unix:/tmp/qemu-monitor-socket,server,nowait `# allow one to interact with qemu-monitor through UNIX socket` \
-	-boot order=d  `# boot from cdrom` \
-	-cdrom "${iso}" \
-	-net user `# configure netwrok interface` \
-	-nic user,hostfwd=tcp::2222-:22,hostfwd=tcp::6443-:6443 `# map ssh and kubernetes ports from host to guest` \
-  -smp "cpus=${VM_CORES}" \
-	-m "${VM_MEM}G" \
+	-chardev "stdio,mux=on,id=char0,logfile=/tmp/${VM_NAME}.log,signal=off" `# help writing output to file` \
+	-mon chardev=char0 `# help writing output to file` \
+	-serial chardev:char0 `# help writing output to file` \
+	-monitor "unix:/tmp/qemu-monitor-socket-${VM_NAME},server,nowait" `# allow one to interact with qemu-monitor through UNIX socket` \
+	-boot "order=d"  `# boot from cdrom` \
+	-cdrom "${iso}" `# boot from cdrom` \
+	-net "user" `# configure netwrok interface` \
+	-nic "user,hostfwd=tcp::2222-:22,hostfwd=tcp::6443-:6443" `# map ssh and kubernetes ports from host to guest` \
+  -smp "cpus=${VM_CORES}" `# set number of cores` \
+	-m "${VM_MEM}G" `# allocate memory` \
 	-virtfs "local,path="${workdir}",mount_tag=hostshare,security_model=none" `# allow for mounting a directory from host into guest, using "hostshare" tag` \
-	-name "${VM_NAME},process=qemu-vm-${VM_NAME}"  \
+	-name "${VM_NAME},process=qemu-vm-${VM_NAME}"  `# set VM name and process name` \
 	"${qcow}" ;
 
 printf "Waiting for SSH to become available ..."
-
 until sshpass -p 'arch' ssh \
 		-p 2222 \
+		-o "ConnectTimeout=10" \
 		-m "hmac-sha2-512" \
 		-o "StrictHostKeyChecking=no" \
 		-o "CheckHostIP=no" \
 		-o "UserKnownHostsFile=/dev/null" \
-		-t "whoami && exit" \
-		"arch@127.0.0.1"  &>/dev/null; do
-	printf "."
-	sleep 2
+		"arch@127.0.0.1" -t "whoami && exit"  &>/dev/null; do
+	printf "." ;
 done
 printf "Waiting unitl cloud-init is done configuring "${VM_NAME}" Guest ..."
 sshpass -p 'arch' ssh \
@@ -231,6 +241,5 @@ sshpass -p 'arch' ssh \
 		-o "StrictHostKeyChecking=no" \
 		-o "CheckHostIP=no" \
 		-o "UserKnownHostsFile=/dev/null" \
-		-t "cloud-init status --wait" \
-		"arch@127.0.0.1" ;
+		"arch@127.0.0.1" -t "cloud-init status --wait";
 printf ""${VM_NAME}" Guest is ready ..."
